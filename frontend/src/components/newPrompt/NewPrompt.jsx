7c2dd183410783541e074a0e94ad748c791e9655
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-07-26 11:36:10 +0530

    add image upload feature

--- frontend/src/components/newPrompt/NewPrompt.jsx
+++ frontend/src/components/newPrompt/NewPrompt.jsx
@@ -1,27 +1,42 @@
-import { useEffect, useRef } from 'react'
-import './newPrompt.css'
+import { useEffect, useRef, useState } from "react";
+import "./newPrompt.css";
+import Upload from "../upload/Upload";
+import { IKImage } from "imagekitio-react";
 
 const NewPrompt = () => {
-    const endRef = useRef(null)
+  const [img, setImg] = useState({
+    isLoading: false,
+    error: "",
+    dbData: {},
+  });
 
-    useEffect(() => {
-      endRef.current.scrollIntoView({behavior:"smooth"})
-    }, [])
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
   return (
-      <>
-          <div className="endChat" ref={endRef}></div>
-          <form className='newForm'>
-              <label htmlFor="file">
-                  <img src="/attachment.png" alt="" />
-              </label>
-              <input id='file' type="file" multiple={false} hidden />
-              <textarea type="text" placeholder='Ask anything...' />
-              <button>
-                  <img src="/arrow.png" alt="" />
-              </button>
-          </form>
+    <>
+      {img.isLoading && <div className="">Loading...</div>}
+      {img.dbData?.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={img.dbData?.filePath}
+          width="380"
+          transformation={[{width: 380}]}
+        />
+      )}
+      <div className="endChat" ref={endRef}></div>
+      <form className="newForm">
+        <Upload setImg={setImg} />
+        <input id="file" type="file" multiple={false} hidden />
+        <textarea type="text" placeholder="Ask anything..." />
+        <button>
+          <img src="/arrow.png" alt="" />
+        </button>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default NewPrompt
+export default NewPrompt;
